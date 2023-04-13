c84115fccc93a0b692125628ce29ad9e73c5d856
Mikhail Sartakov <m.sartakov@g.nsu.ru>
2023-04-13 13:51:04 +0700

    added main

---
+++ lab2 (2048 game)/game/src/main/java/ru/nsu/fit/mihanizzm/game2048/GameMain.java
@@ -0,0 +1,15 @@
+package ru.nsu.fit.mihanizzm.game2048;
+
+import ru.nsu.fit.mihanizzm.game2048.model.*;
+import ru.nsu.fit.mihanizzm.game2048.presenter.GamePresenter;
+import ru.nsu.fit.mihanizzm.game2048.view.GameFrame;
+import ru.nsu.fit.mihanizzm.game2048.view.GameView;
+
+public class GameMain {
+    public static void main(String[] args) {
+//        GameField field = new GameField(4);
+        GameView view = new GameFrame(4);
+//        GamePresenter presenter = new GamePresenter(field, view);
+//        presenter.run();
+    }
+}
